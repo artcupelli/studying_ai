3301ef23c19658b69d27387369c91629ad2609b6
Artur Cupelli <artur.cupelli@usp.br>
2023-04-13 12:00:25 -0300

    feat: Read useful columns of the dataset

--- artificial_neural_network/main.py
+++ artificial_neural_network/main.py
@@ -2,4 +2,11 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-print(tf.__version__)
+DATASET_PATH = 'Churn_Modelling.csv'
+
+## Read the dataset
+dataset = pd.read_csv(DATASET_PATH)
+
+## Filter only necessary features, remove columns like: ID, customer name...
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
