72dbc16631cf33fcf00c456432cc5538e9b3ea76
Artur Cupelli <artur.cupelli@usp.br>
2023-04-13 12:19:04 -0300

    feat: Transform categorical data into numeric values

--- artificial_neural_network/main.py
+++ artificial_neural_network/main.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 DATASET_PATH = 'Churn_Modelling.csv'
 
@@ -10,3 +13,12 @@ dataset = pd.read_csv(DATASET_PATH)
 ## Filter only necessary features, remove columns like: ID, customer name...
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
+
+## Encode categorical data (category to number)
+# 'Gender' column
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+# 'Geography' column (One-hot encoding)
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
